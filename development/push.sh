baa7f4c29e7d20d60dd063962c61bb0be7b33601
Evan Summers <evan.summers@gmail.com>
2017-01-30 23:32:53 +0000

    initial

--- development/push.sh
+++ development/push.sh
@@ -1,7 +1,7 @@
 
   rm -rf tmp/data
-  echo '{"twitter": "@evanxsummers"}' | jq '.' | redis-cli -x set evanxsummers
-  redis-cli lpush r8:q evanxsummers
+  echo '{"twitter": "@evanxsummers"}' | jq '.' | redis-cli -x set user:evanxsummers
+  redis-cli lpush r8:q user:evanxsummers
   echo '[1, 2]' | jq '.' | redis-cli -x set test123
   redis-cli lpush r8:q test123
   echo '[1, 2, 3]' | jq '.' | redis-cli -x set test123
