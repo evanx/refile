474938d584c8e7365ec6ef2a5706586242780ae9
Evan Summers <evan.summers@gmail.com>
2017-02-14 01:07:21 +0000

    initial

--- test/demo.sh
+++ test/demo.sh
@@ -17,8 +17,6 @@
   docker network create -d bridge test-r8-network
   redisContainer=`docker run --network=test-r8-network \
       --name test-r8-redis -d redis`
-  password=`docker logs $redisContainer | grep '^\s*redis-cli -a' |
-      sed -e 's/^\s*redis-cli -a \(\w*\) .*$/\1/'`
   redisHost=`docker inspect $redisContainer |
       grep '"IPAddress":' | tail -1 | sed 's/.*"\([0-9\.]*\)",/\1/'`
   dd if=/dev/urandom bs=32 count=1 > $HOME/volumes/test-r8/spiped-keyfile
@@ -34,12 +32,13 @@
     -e -s "[0.0.0.0]:6333" -t "[$decipherHost]:6444"`
   encipherHost=`docker inspect $encipherContainer |
     grep '"IPAddress":' | tail -1 | sed 's/.*"\([0-9\.]*\)",/\1/'`
-  redis-cli -a $password -h $encipherHost -p 6333 set user:evanxsummers '{"twitter":"evanxsummers"}'
-  redis-cli -a $password -h $encipherHost -p 6333 lpush r8:q user:evanxsummers
+  redis-cli -h $encipherHost -p 6333 set user:evanxsummers '{"twitter":"evanxsummers"}'
+  redis-cli -h $encipherHost -p 6333 lpush r8:q user:evanxsummers
   appContainer=`docker run --name test-r8-app -d \
     --network=test-r8-network \
     -v $HOME/volumes/test-r8/data:/data \
-    -e host=$encipherHost -e port=6333 -e password=$password \
+    -e host=$encipherHost \
+    -e port=6333 \
     evanxsummers/r8`
   sleep 1
   docker logs $appContainer
